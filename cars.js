d4240d9caa648dac23b2d4a4a7a01a22d4715a5e
Matt Munch <mattmunch94@gmail.com>
2019-10-07 16:59:24 -0700

    initial commit

---
+++ cars.js
@@ -0,0 +1,57 @@
+const mustang = {
+    code: 'mustang',
+    name: 'Ford Mustang',
+    image: '../assets/mustang.jpeg',
+    description: "Ford's flagship muscle car",
+    category: 'muscle',
+    price: 28000
+};
+const subSti = {
+    code: 'subSti',
+    name: 'Subaru STI',
+    image: '../asset/subaruSTI.jpg',
+    description: "Subaru's most popular sports car",
+    category: 'import',
+    price: 43000
+};
+const mitsEvo = {
+    code: 'mitsEvo',
+    name: 'Mitsubishi Evolution',
+    image: '../assets/mitsubishiEVO.jpg',
+    description: "Mitsubishi's most popular sports car",
+    category: 'import',
+    price: 38000
+};
+const hellcat = {
+    code: 'hellcat',
+    name: 'Dodge Hellcat',
+    image: '../assets/hellcat.jpg',
+    description: "Dodge's flagship muscle car",
+    category: 'muscle',
+    price: 60000
+};
+const nissanGtr = {
+    code: 'nissanGtr',
+    name: 'Nissan GTR',
+    image: '../assets/nissanGTR.jpg',
+    description: "Nissan's most sought after sports car",
+    category: 'import',
+    price: 114000
+};
+const supra = {
+    code: 'supra',
+    name: 'Toyota Supra',
+    image: '../assets/toyotasupra.jpg',
+    description: "Toyota's flagship sports car",
+    category: 'import',
+    price: 50000
+};
+const cars = [
+    supra,
+    nissanGtr,
+    hellcat,
+    mitsEvo,
+    subSti,
+    mustang
+];
+export default cars;
